600df1c2b74c4240b7475a4c5fb393560d5aa227
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2023-05-31 17:46:31 +0200

    use RNAbility

--- tester/harmony/entry/src/main/ets/entryability/EntryAbility.ts
+++ tester/harmony/entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,41 +1,19 @@
 import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
+import { RNAbility } from '@ohos/rnoh/ts';
 
-export default class EntryAbility extends UIAbility {
-    onCreate(want, launchParam) {
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
-    }
-
-    onDestroy() {
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
-    }
-
+export default class EntryAbility extends RNAbility {
     onWindowStageCreate(windowStage: window.WindowStage) {
         // Main window is created, set main page for this ability
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
+        hilog.info(0x0000, 'RNOH', '%{public}s', 'Ability onWindowStageCreate');
 
         windowStage.loadContent('pages/Index', (err, data) => {
             if (err.code) {
-                hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
+                hilog.error(0x0000, 'RNOH', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
                 return;
             }
-            hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
+            hilog.info(0x0000, 'RNOH', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
         });
-    }
-
-    onWindowStageDestroy() {
-        // Main window is destroyed, release UI related resources
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageDestroy');
-    }
-
-    onForeground() {
-        // Ability has brought to foreground
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onForeground');
-    }
-
-    onBackground() {
-        // Ability has back to background
-        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onBackground');
     }
 };
