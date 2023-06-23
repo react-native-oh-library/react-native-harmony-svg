56e75988bb704774cbff3c361a528eb89639c765
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2023-06-23 18:06:28 +0200

    update RN

--- tester/harmony/entry/src/main/ets/entryability/EntryAbility.ts
+++ tester/harmony/entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,19 +1,12 @@
-import UIAbility from '@ohos.app.ability.UIAbility';
-import hilog from '@ohos.hilog';
-import window from '@ohos.window';
 import { RNAbility } from '@ohos/rnoh/ts';
 
 export default class EntryAbility extends RNAbility {
-    onWindowStageCreate(windowStage: window.WindowStage) {
-        // Main window is created, set main page for this ability
-        hilog.info(0x0000, 'RNOH', '%{public}s', 'Ability onWindowStageCreate');
+    getPagePath() {
+        return "pages/Index"
+    }
 
-        windowStage.loadContent('pages/Index', (err, data) => {
-            if (err.code) {
-                hilog.error(0x0000, 'RNOH', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
-                return;
-            }
-            hilog.info(0x0000, 'RNOH', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
-        });
+    getBundleURL() {
+        return "http://localhost:8081/index.bundle?platform=harmony&dev=false&minify=false"
     }
 };
+
