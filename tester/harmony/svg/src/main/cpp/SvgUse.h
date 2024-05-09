e95f671c60975a97cb4a868bd8b4688e96451361
seventeen18741 <chenximing6@huawei.com>
2024-05-09 09:32:46 +0800

    add use and defs component

---
+++ tester/harmony/svg/src/main/cpp/SvgUse.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "SvgGraphic.h"
+#include <native_drawing/drawing_point.h>
+#include <native_drawing/drawing_canvas.h>
+#include <native_drawing/drawing_path.h>
+
+namespace rnoh {
+
+    class SvgUse : public SvgGraphic {
+    public:
+        SvgUse() = default;
+        ~SvgUse() override = default;
+        float x;
+        float y;
+        float width;
+        float height;
+        float opacity;
+        std::string href;
+
+        void OnDraw(OH_Drawing_Canvas *canvas) override;
+    private:
+        class AttributeScope {
+            public:
+                explicit AttributeScope(const std::shared_ptr<SvgNode>& node);
+                ~AttributeScope();
+
+            private:
+                SvgBaseAttribute attributes_;
+                std::shared_ptr<SvgNode> node_;
+            };
+                // SvgAttributes useAttr_;
+        };
+
+} // namespace rnoh
