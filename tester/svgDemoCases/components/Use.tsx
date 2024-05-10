3b2b616fe5e734918acc65c6a32e07b1e3771c44
seventeen18741 <chenximing6@huawei.com>
2024-05-10 20:38:26 +0800

    test: Add testcase about Defs and Use

--- tester/svgDemoCases/components/Use.tsx
+++ tester/svgDemoCases/components/Use.tsx
@@ -1,22 +1,19 @@
 import React from "react";
-import { Svg, Circle,  } from 'react-native-svg'
+import { Svg, Circle, Use, Defs, G} from 'react-native-svg'
 import { ScrollView, View, Text } from 'react-native'
 import {Tester, Filter, TestCase, TestSuite } from '@rnoh/testerino';
 export default function () {
     return (
-        <Tester style={{flex: 1}}>
-             <ScrollView>
-                <TestCase 
-                    itShould="case 1 desc"
-                >
-                    <Text>case 1 in here</Text>
-                </TestCase>
-                <TestCase 
-                    itShould="case 2 desc"
-                >
-                    <Text>case 2 in here</Text>
-                </TestCase>
-            </ScrollView>
-        </Tester>
+        <Svg>
+            <Circle  cx="50" cy="50" r="50" fill="black" />
+
+            <Defs>
+                <G id ="shape1">
+                    <Circle  cx="200" cy="200" r="50" fill="black" />
+                </G>
+            </Defs>
+
+            <Use href="#shape1" x="100" y="100" />
+        </Svg>
     )
 }
