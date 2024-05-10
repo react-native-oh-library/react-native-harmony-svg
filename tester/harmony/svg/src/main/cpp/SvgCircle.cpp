24c7243f29f1bd0f3549bc95e2646103f011804d
Louis-C7 <chenlixi1@huawei.com>
2024-05-10 19:55:16 +0800

    feat: implement Circle using AsPath

--- tester/harmony/svg/src/main/cpp/SvgCircle.cpp
+++ tester/harmony/svg/src/main/cpp/SvgCircle.cpp
@@ -16,34 +16,12 @@
 #include "SvgCircle.h"
 
 namespace rnoh {
-void SvgCircle::OnDraw(OH_Drawing_Canvas *canvas) {
-    LOG(INFO) << "[SVGNode] OnCircle:";
-    auto brush = OH_Drawing_BrushCreate();
 
-    OH_Drawing_BrushSetColor(brush, colorFill);
-    OH_Drawing_BrushSetAlpha(brush, opacity);
-
-    OH_Drawing_CanvasAttachBrush(canvas, brush);
-    auto point = OH_Drawing_PointCreate(vpToPx(x), vpToPx(y));
-    // 使用 props属性生成路径。
-    OH_Drawing_CanvasDrawCircle(canvas, point, vpToPx(r));
-    OH_Drawing_PointDestroy(point);
-}
-
-//  OH_Drawing_Path* SvgCircle:: AsPath()  {
-//      auto path = OH_Drawing_PathCreate();
-//      // 使用 props属性生成路径。
-//      float width = width_;
-//      float height = height_;
-//      OH_Drawing_PathMoveTo(path, width / 4, height / 4);
-//      OH_Drawing_PathLineTo(path, width * 3 / 4, height / 4);
-//      OH_Drawing_PathLineTo(path, width * 3 / 4, height * 3 / 4);
-//      OH_Drawing_PathLineTo(path, width / 4, height * 3 / 4);
-//      OH_Drawing_PathLineTo(path, width / 4, height / 4);
-//
-//
-//      OH_Drawing_PathClose(path);
-//      return path;
-//  }
+ OH_Drawing_Path* SvgCircle:: AsPath()  {
+     LOG(INFO) << "[SvgCircle] AsPath";
+     // TODO implement ConvertDimensionToPx
+          OH_Drawing_PathAddCircle(path_, vpToPx(x), vpToPx(y), vpToPx(r), PATH_DIRECTION_CW);
+     return path_;
+ }
 
 } // namespace rnoh
