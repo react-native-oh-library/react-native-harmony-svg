d897e50ec29e63cd63f099589baa67ffe163c670
seventeen18741 <chenximing6@huawei.com>
2024-05-11 17:07:41 +0800

    fix: Complement Use component by Symbol

--- tester/harmony/svg/src/main/cpp/SvgSymbol.h
+++ tester/harmony/svg/src/main/cpp/SvgSymbol.h
@@ -2,39 +2,48 @@
 
 #include <string>
 #include "SvgNode.h"
+#include "SvgGraphic.h"
 #include "properties/Rect.h"
+#include "properties/ViewBox.h"
+#include <native_drawing/drawing_canvas.h>
 
 namespace rnoh {
 
 class SvgSymbol : public SvgNode {
 
 public:
-    std::string id;
-    Rect viewBox;
-
     SvgSymbol() : SvgNode()
     {
-        InitDefsFlag();
-        
-    }
+        InitSymbolFlag();
 
+    }
     ~SvgSymbol() override = default;
 
-    void InitDefsFlag()
+    void InitSymbolFlag()
     {
-        hrefFill_ = false;
-        hrefRender_ = false;
-        inheritStyle_ = false;
-        drawTraversed_ = false;
+        // hrefFill_ = false;
+        // hrefRender_ = false;
+        // inheritStyle_ = false;
+        // drawTraversed_ = true;
     }
 
-    OH_Drawing_Path *AsPath() override
-    {
-        OH_Drawing_Path *path;
-        for (auto child : children_) {
-            path = child->AsPath();
+    float mMinX;
+    float mMinY;
+    float mVbWidth;
+    float mVbHeight;
+    std::string mAlign;
+    int mMeetOrSlice;
+
+    void drawSymbol(OH_Drawing_Canvas *canvas, float width, float height) {
+        LOG(INFO) << "[SvgUse::SvgSymbol] : drawSymbol width :<< "<< width << " height:"<< height;
+        if (!mAlign.empty()) {
+            LOG(INFO) << "[SvgUse::SvgSymbol] : drawSymbol start" ;
+            Rect vbRect(mMinX * scale_, mMinY * scale_, (mMinX + mVbWidth) * scale_, (mMinY + mVbHeight) * scale_);
+            Rect eRect(0, 0, vpToPx(width), vpToPx(height));
+            OH_Drawing_Matrix *viewBoxMatrix = rhon::ViewBox::getTransform(vbRect, eRect, mAlign, mMeetOrSlice);
+            OH_Drawing_CanvasConcatMatrix(canvas, viewBoxMatrix);
+            // Draw(canvas);
         }
-        return path;
     }
 };
 
