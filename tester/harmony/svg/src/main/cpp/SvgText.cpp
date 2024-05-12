b1889ef8ffef14a3f1074498a58176b668795d5c
Tianer Zhou <zhoutianer@huawei.com>
2024-05-12 11:53:20 +0800

    dx dy naive implementation
    Signed-off-by: Tianer Zhou <zhoutianer@huawei.com>

--- tester/harmony/svg/src/main/cpp/SvgText.cpp
+++ tester/harmony/svg/src/main/cpp/SvgText.cpp
@@ -9,11 +9,7 @@
 namespace rnoh {
 void SvgText::OnDraw(OH_Drawing_Canvas *canvas) {
     if (!glyphCtx_) {
-        auto* rect = OH_Drawing_RectCreate(0, 0, 0, 0);
-        OH_Drawing_CanvasGetLocalClipBounds(canvas, rect);
-        glyphCtx_ = std::make_shared<GlyphContext>(scale_, OH_Drawing_RectGetWidth(rect), OH_Drawing_RectGetHeight(rect));
-        LOG(INFO) << "clip width = " << OH_Drawing_RectGetWidth(rect) << " clip height = " << OH_Drawing_RectGetHeight(rect);
-        OH_Drawing_RectDestroy(rect);
+        InitGlyph(canvas);
     }
     glyphCtx_->pushContext(true, shared_from_this(), {}, x_, y_, dx_, dy_, rotate_);
     for (auto const &child : children_) {
@@ -32,5 +28,15 @@ void SvgText::OnDraw(OH_Drawing_Canvas *canvas) {
         // }
         tSpan->SetContext(glyphCtx_);
     }
+}
+
+void SvgText::InitGlyph(OH_Drawing_Canvas *canvas) {
+    auto rect = std::unique_ptr<OH_Drawing_Rect, decltype(&OH_Drawing_RectDestroy)>(OH_Drawing_RectCreate(0, 0, 0, 0),
+                                                                                     OH_Drawing_RectDestroy);
+    // auto *rect = OH_Drawing_RectCreate(0, 0, 0, 0);
+    OH_Drawing_CanvasGetLocalClipBounds(canvas, rect.get());
+    glyphCtx_ = std::make_shared<GlyphContext>(scale_, OH_Drawing_RectGetWidth(rect.get()), OH_Drawing_RectGetHeight(rect.get()));
+    LOG(INFO) << "TEXT clip width = " << OH_Drawing_RectGetWidth(rect.get())
+              << " clip height = " << OH_Drawing_RectGetHeight(rect.get());
 }
 } // namespace rnoh
