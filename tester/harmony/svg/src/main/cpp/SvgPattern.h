4d2500a106dc7bc1b8db5f97a079e39f6b3a385b
shinyxhh <9450ave@163,com>
2024-05-13 22:15:05 +0800

    feat: add attributes and tests of pattern

--- tester/harmony/svg/src/main/cpp/SvgPattern.h
+++ tester/harmony/svg/src/main/cpp/SvgPattern.h
@@ -9,14 +9,47 @@ namespace rnoh {
 class SvgPattern : public SvgQuote {
 
 public:
-    SvgPatternAttribute patternAttr_;
-    
+    using Float = facebook::react::Float;
     SvgPattern();
     ~SvgPattern() = default;
     
     virtual void OnDrawTraversedBefore(OH_Drawing_Canvas *canvas) override;
     virtual void OnDrawTraversedAfter(OH_Drawing_Canvas *canvas) override;
-    
-};
+    void SaveDefinition(OH_Drawing_Canvas *canvas);
+    void ConcatMatrix(OH_Drawing_Canvas *canvas);
+    const Pattern &GetPatternAttr() const;
 
+    void setPatternX(Dimension x);
+
+    void setPatternY(Dimension y);
+
+    void setHeight(Dimension height);
+
+    void setWidth(Dimension width);
+
+    void setPatternUnits(int patternUnits);
+
+    void setPatternContentUnits(int patternContentUnits);
+
+    void setViewBox(Rect viewBox);
+
+    void setImage(OH_Drawing_Image *image);
+
+    void setmMinX(float mMinX);
+
+    void setmMinY(float mMinUY);
+
+    void setmVbWidth(float mVbWidth);
+
+    void setmVbHeight(float mVbHeight);
+
+    void setmAlign(std::string mAlign);
+
+    void setmMeetOrSlice(int mMeetOrSlice);
+
+    void setPatternTransforms(std::vector<Float> gradientTransforms);
+private:
+    Pattern pattern;
+    double scale_ = 3.25010318;
+  };
 }
