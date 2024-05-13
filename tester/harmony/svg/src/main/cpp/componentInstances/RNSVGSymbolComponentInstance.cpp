cc349567e3d825ece34c5aa572e9e30e4906cab2
lisibo <leacyb@163.com>
2024-05-13 10:29:34 +0800

    fix symbol

--- tester/harmony/svg/src/main/cpp/componentInstances/RNSVGSymbolComponentInstance.cpp
+++ tester/harmony/svg/src/main/cpp/componentInstances/RNSVGSymbolComponentInstance.cpp
@@ -13,22 +13,26 @@ RNSVGSymbolComponentInstance::RNSVGSymbolComponentInstance(Context context) : Cp
 
 void RNSVGSymbolComponentInstance::onPropsChanged(SharedConcreteProps const &props) {
     CppComponentInstance::onPropsChanged(props);
-    GetSvgNode()->UpdateCommonProps(props);
-    LOG(INFO) << "[RNSVGSymbolComponentInstance] minX: " << props->minX;
-    LOG(INFO) << "[RNSVGSymbolComponentInstance] minY: " << props->minY;
-    LOG(INFO) << "[RNSVGSymbolComponentInstance] vbWidth: " << props->vbWidth;
-    LOG(INFO) << "[RNSVGSymbolComponentInstance] vbHeight: " << props->vbHeight;
-    LOG(INFO) << "[RNSVGSymbolComponentInstance] mAlign: " << props->align;
-    LOG(INFO) << "[RNSVGSymbolComponentInstance] meetOrSlice: " << props->meetOrSlice;
-    // set attribute to svgSymbol
+
+    
     auto svgSymbol = std::dynamic_pointer_cast<SvgSymbol>(GetSvgNode());
+    // set attribute to svgMask.
     svgSymbol->mMinX = props->minX;
     svgSymbol->mMinY = props->minY;
-    svgSymbol->mVbHeight = props->vbWidth;
+    svgSymbol->mVbWidth = props->vbWidth;
     svgSymbol->mVbHeight = props->vbHeight;
     svgSymbol->mMeetOrSlice = props->meetOrSlice;
     svgSymbol->mAlign = props->align;
-    // svgSymbol->viewBox = Rect(props->minX, props->minY, props->vbWidth, props->vbHeight);
+    
+    LOG(INFO) << "[SvgSymbol RNSVGSymbolComponentInstance] minX: " << props->minX;
+    LOG(INFO) << "[SvgSymbol RNSVGSymbolComponentInstance] minY: " << props->minY;
+    LOG(INFO) << "[SvgSymbol RNSVGSymbolComponentInstance] vbWidth: " << props->vbWidth;
+    LOG(INFO) << "[SvgSymbol RNSVGSymbolComponentInstance] vbHeight: " << props->vbHeight;
+    LOG(INFO) << "[SvgSymbol RNSVGSymbolComponentInstance] mMeetOrSlice: " << props->meetOrSlice;
+    LOG(INFO) << "[SvgSymbol RNSVGSymbolComponentInstance] mAlign: " << props->align;
+    // set attribute to svgSymbol
+    //auto svgSymbol = std::dynamic_pointer_cast<SvgSymbol>(GetSvgNode());
+   // svgSymbol->viewBox = Rect(props->minX, props->minY, props->vbWidth, props->vbHeight);
     GetSvgNode()->UpdateCommonProps(props);
 }
 
--- tester/svgDemoCases/components/Symbol.tsx
+++ tester/svgDemoCases/components/Symbol.tsx
@@ -1,58 +1,35 @@
 import React from "react";
-import { Svg, Circle, SvgFromXml, Use, Symbol } from 'react-native-svg'
+import { Svg, Circle, SvgFromXml, Symbol, Use } from 'react-native-svg'
 import { ScrollView, View, Text } from 'react-native'
 import { Tester, Filter, TestCase, TestSuite } from '@rnoh/testerino';
 import { opacity } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
 import { transformer } from "../../metro.config";
 export default function () {
     return (
-    <Tester style={{ flex: 1 }}>
-        <ScrollView>
-        <TestCase
-            itShould="case 1: display symbol with use "
-        >
-            <View style={{
+        <Tester style={{ flex: 1 }}>
+            <ScrollView>
+                <TestCase
+                    itShould="case 1 desc"
+                >
+                    <View
+                        style={{
                             borderWidth: 1,
-                            width: 100,
-                            height: 100
+                            width: 200,
+                            height: 300
                         }}
-            >
-                <Svg viewBox="0 0 400 400">
-                    <Circle id="shape" cx="100" cy="100" r="20" fill="black" />
-
-                    <Use href="#shape" x="200" y="200" fill="blue" />
-                </Svg>
-            </View>
-        </TestCase>
-
-        <TestCase
-            itShould="case 2: TODO: dismatch while use viewbox"
-        >
-            <View style={{
-                            borderWidth: 1,
-                            width: 100,
-                            height: 100
-                        }}
-            >
-                <Svg viewBox="0 0 400 400">
-                <Symbol id="symbol" viewBox="0 0 150 110" width="100" height="50">
-                    <Circle cx="50" cy="50" r="40" strokeWidth="8" stroke="red" fill="red" />
-                    <Circle
-                    cx="90"
-                    cy="60"
-                    r="40"
-                    strokeWidth="8"
-                    stroke="green"
-                    fill="white"
-                    />
-                </Symbol>
-                    <Use href="#symbol" x="0" y="0" />
-                    <Use href="#symbol" x="0" y="50" width="75" height="38" />
-                    <Use href="#symbol" x="0" y="100" width="50" height="25" />
-                </Svg>
-            </View>
-        </TestCase>
-        </ScrollView>
-    </Tester>
+                    >
+                        <Svg>
+                            <Symbol id="sym01" viewBox="0 0 100 110">
+                                <Circle cx="80" cy="50" r="40" strokeWidth="8" stroke="green" fill="red" />
+                                <Circle cx="130" cy="60" r="40" strokeWidth="8" stroke="green" fill="white" />
+                            </Symbol>
+                            <Use href="#sym01" x="0" y="0" width="100" height="50" />
+                            <Use href="#sym01" x="0" y="50" width="75" height="38" />
+                            <Use href="#sym01" x="0" y="100" width="50" height="25" />
+                        </Svg>
+                    </View>
+                </TestCase>
+            </ScrollView>
+        </Tester>
     )
 }
