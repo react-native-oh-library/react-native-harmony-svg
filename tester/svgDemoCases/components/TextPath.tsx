13e324ac03a6f9f1aee18c34420f1711435dcd5c
lizhenyu46 <lizhenyu46@huawei.com>
2024-05-14 16:20:48 +0800

    test: add TextPath href and startOffset test

--- tester/svgDemoCases/components/TextPath.tsx
+++ tester/svgDemoCases/components/TextPath.tsx
@@ -1,22 +1,89 @@
 import React from "react";
 import { Svg, Text, TextPath, Path, Defs, G } from 'react-native-svg'
 import { ScrollView, View } from 'react-native'
-import { Tester, Filter, TestCase, TestSuite } from '@rnoh/testerino';
+import { GenMain } from './gen'
+import { genTransformProps, genStrokeProps, genFillProps, CaseParams } from '../genUtil'
+import {Tester, Filter, TestCase, TestSuite } from '@rnoh/testerino';
+
 export default function () {
     return (
-        <Svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-            <G stroke="blue">
-                <Path
-                    id="MyPath"
-                    fill="none"
-                    stroke="red"
-                    d="M10,90 Q90,90 90,45 Q90,10 50,10 Q10,10 10,40 Q10,70 45,70 Q70,70 75,50" />
+        <Tester style={{ flex: 1 }}>
+            <ScrollView>
+                <TestCase
+                    itShould="case 1: TextPath use href "
+                >
+                    <View style={{
+                                    borderWidth: 1,
+                                    width: 100,
+                                    height: 100
+                                }}
+                    >
+                        <Svg viewBox="0 0 100 100">
+                            <G stroke="blue">
+                                <Path
+                                    id="MyPath"
+                                    fill="none"
+                                    stroke="red"
+                                    d="M10,90 Q90,90 90,45 Q90,10 50,10 Q10,10 10,40 Q10,70 45,70 Q70,70 75,50" />
 
-                <Text color="white" stroke="blue" fill="blue" fontVariant="small-caps" fontStretch="ultra-expanded" fontVariationSettings={"XHGT 0.7"}>
-                <TextPath stroke="blue" fill="blue" href="#MyPath">Quick brown fox jumps over the lazy dog.</TextPath>
-            </Text>
-        </G>
-        </Svg >
+                                <Text stroke="blue" fill="blue" fontVariant="small-caps" fontStretch="ultra-expanded" fontVariationSettings={"XHGT 0.7"}>
+                                <TextPath stroke="blue" fill="blue" href="#MyPath">Quick brown fox jumps over the lazy dog.</TextPath>
+                                </Text>
+                            </G>
+                        </Svg >
+                    </View>
+                </TestCase>
 
+                <TestCase
+                    itShould="case 2: TextPath use startOffset number "
+                >
+                    <View style={{
+                                    borderWidth: 1,
+                                    width: 100,
+                                    height: 100
+                                }}
+                    >
+                        <Svg viewBox="0 0 100 100">
+                            <G stroke="blue">
+                                <Path
+                                    id="MyPath"
+                                    fill="none"
+                                    stroke="red"
+                                    d="M10,90 Q90,90 90,45 Q90,10 50,10 Q10,10 10,40 Q10,70 45,70 Q70,70 75,50" />
+
+                                <Text stroke="blue" fill="blue" fontVariant="small-caps" fontStretch="ultra-expanded" fontVariationSettings={"XHGT 0.7"}>
+                                <TextPath stroke="blue" fill="blue" href="#MyPath" startOffset="50">Quick brown fox jumps over the lazy dog.</TextPath>
+                                </Text>
+                            </G>
+                        </Svg >
+                    </View>
+                </TestCase>
+
+                <TestCase
+                    itShould="case 3: TextPath use startOffset percent "
+                >
+                    <View style={{
+                                    borderWidth: 1,
+                                    width: 100,
+                                    height: 100
+                                }}
+                    >
+                        <Svg viewBox="0 0 100 100">
+                            <G stroke="blue">
+                                <Path
+                                    id="MyPath"
+                                    fill="none"
+                                    stroke="red"
+                                    d="M10,90 Q90,90 90,45 Q90,10 50,10 Q10,10 10,40 Q10,70 45,70 Q70,70 75,50" />
+
+                                <Text stroke="blue" fill="blue" fontVariant="small-caps" fontStretch="ultra-expanded" fontVariationSettings={"XHGT 0.7"}>
+                                <TextPath stroke="blue" fill="blue" href="#MyPath" startOffset="50%">Quick brown fox jumps over the lazy dog.</TextPath>
+                                </Text>
+                            </G>
+                        </Svg >
+                    </View>
+                </TestCase>
+            </ScrollView>
+        </Tester>
     )
 }
