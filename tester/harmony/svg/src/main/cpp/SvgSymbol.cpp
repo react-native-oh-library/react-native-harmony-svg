980fefd33c01a97f58cb22c58426e464d271050e
leecoob <leacyb@163.com>
2024-05-16 14:39:07 +0800

    fix:modify symbol component

--- tester/harmony/svg/src/main/cpp/SvgSymbol.cpp
+++ tester/harmony/svg/src/main/cpp/SvgSymbol.cpp
@@ -10,8 +10,8 @@ void SvgSymbol::drawSymbol(OH_Drawing_Canvas *canvas, float width, float height)
     LOG(INFO) << "[SvgSymbol::SvgSymbol] : drawSymbol width :<< " << width << " height:" << height
               << " mAlign : " << mAlign;
     if (!mAlign.empty()) {
-        Rect vbRect(mMinX * scale_, mMinY * scale_, (mMinX + mVbWidth) * scale_, (mMinY + mVbHeight) * scale_);
-        Rect eRect(0, 0, width / scale_, height / scale_);
+        Rect vbRect(vpToPx(mMinX), vpToPx(mMinY), vpToPx(mMinX + mVbWidth), vpToPx(mMinY + mVbHeight));
+        Rect eRect(0,0, vpToPx(width), vpToPx(height));
         OH_Drawing_Matrix *viewBoxMatrix = ViewBox::getTransform(vbRect, eRect, mAlign, mMeetOrSlice);
         OH_Drawing_CanvasConcatMatrix(canvas, viewBoxMatrix);
         Draw(canvas);
