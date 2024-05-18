5091e3a706731fa72ccfa66133d5c00616ae23a1
Louis Chan <61399097+Louis-C7@users.noreply.github.com>
2024-05-19 00:40:32 +0800

    Revert "feat: add svgImage props (x, y, width, height, opacity, href)"

--- tester/harmony/svg/src/main/cpp/componentInstances/RNSVGImageComponentInstance.cpp
+++ tester/harmony/svg/src/main/cpp/componentInstances/RNSVGImageComponentInstance.cpp
@@ -2,28 +2,20 @@
 #include "Props.h"
 #include <react/renderer/core/ConcreteState.h>
 #include <sstream>
-#include <SvgImage.h>
+#include "SvgCircle.h"
 
 namespace rnoh {
 namespace svg {
 
-RNSVGImageComponentInstance::RNSVGImageComponentInstance(Context context) : RNSVGBaseComponentInstance(std::move(context)) {
-    SetSvgNode(std::make_shared<SvgImage>());
+RNSVGImageComponentInstance::RNSVGImageComponentInstance(Context context)  : RNSVGBaseComponentInstance(std::move(context)) {
+    SetSvgNode(std::make_shared<SvgCircle>());
 }
 
 void RNSVGImageComponentInstance::UpdateElementProps(SharedConcreteProps const &props) {
-    CppComponentInstance::onPropsChanged(props);
-    GetSvgNode()->UpdateCommonProps(props);
-    auto svgImage = std::dynamic_pointer_cast<SvgImage>(GetSvgNode());
-    svgImage->x = std::stof(props->x);
-    svgImage->y = std::stof(props->y);
-    svgImage->width = std::stof(props->width);
-    svgImage->height = std::stof(props->height);
-    svgImage->opacity = props->opacity;
-    svgImage->src = props->src;
-    svgImage->align = props->align;
-    svgImage->meetOrSlice = props->meetOrSlice;
+      
+      
 }
+
 
 } // namespace svg
 } // namespace rnoh
