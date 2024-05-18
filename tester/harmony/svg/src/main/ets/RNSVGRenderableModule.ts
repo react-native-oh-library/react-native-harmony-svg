9950774f05049f590ad77e213772cabb5ffbb73e
linjiacheng <425765923@qq.com>
2024-05-18 19:58:03 +0800

    feat: implement base RNSVGRenderableModule

---
+++ tester/harmony/svg/src/main/ets/RNSVGRenderableModule.ts
@@ -0,0 +1,5 @@
+import { TurboModule } from '@rnoh/react-native-openharmony/ts';
+
+export class RNSVGRenderableModule extends TurboModule {
+  public static readonly NAME = 'RNSVGRenderableModule';
+}
