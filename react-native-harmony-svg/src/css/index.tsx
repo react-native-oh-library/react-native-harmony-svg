b1164af19358c66a364db2a5907be76a7d8e022f
Louis Chan <61399097+Louis-C7@users.noreply.github.com>
2024-06-17 18:56:44 +0800

    feat: move css to different package for size reduction same as source library (#238)

---
+++ react-native-harmony-svg/src/css/index.tsx
@@ -0,0 +1,20 @@
+import {
+    SvgCss,
+    SvgCssUri,
+    SvgWithCss,
+    SvgWithCssUri,
+    inlineStyles,
+  } from 'react-native-svg/css';
+  
+  import { LocalSvg, WithLocalSvg, loadLocalRawResource } from 'react-native-svg/css';
+  
+  export {
+    SvgCss,
+    SvgCssUri,
+    SvgWithCss,
+    SvgWithCssUri,
+    inlineStyles,
+    LocalSvg,
+    WithLocalSvg,
+    loadLocalRawResource,
+  };
